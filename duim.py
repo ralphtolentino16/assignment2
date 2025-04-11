acebdcb8bb78eefae1281f0a42f60f771ff674c4
ralphtolentino16 <ralph.tolentino16@gmail.com>
2025-04-11 14:12:55 -0400

    First Milestone call_du_sub and percent_to_graph

--- duim.py
+++ duim.py
@@ -23,6 +23,7 @@ Description: This script will implement imporvements when calling du. It will re
 Date: April 11, 2025
 '''
 
+
 def parse_command_args():
     "Set up argparse here. Call this function inside main."
     parser = argparse.ArgumentParser(description="DU Improved -- See Disk Usage Report with bar charts",epilog="Copyright 2022")
@@ -30,17 +31,43 @@ def parse_command_args():
     # add argument for "human-readable". USE -H, don't use -h! -h is reserved for --help which is created automatically.
     # check the docs for an argparse option to store this as a boolean.
     # add argument for "target". set number of args to 1.
-    args = parser.parse_args()
+    parser.add_argument("target", nargs="?", default=".", help="The target directory")
+    return parser.parse_args()
 
 
 def percent_to_graph(percent, total_chars):
     "returns a string: eg. '##  ' for 50 if total_chars == 4"
-    pass
+    "THis function checks if percent is between 0 and 100"
+    "And converts the percent to graph using the = character and empty spcae."
+
+    if not (0 <= percent <= 100):
+        raise ValueError("Percent must be between 0 and 100.")
+    filled = round((percent / 100) * total_chars)
+    empty = total_chars - filled
+    return "=" * filled + " " * empty
 
 def call_du_sub(location):
     "takes the target directory as an argument and returns a list of strings"
     "returned by the command `du -d 1 location`"
-    pass
+    "this fumctioncalls the du command and splits the result into a list"
+
+    try:
+        proc = subprocess.Popen(
+            ["du", "-d", "1", location],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        stdout, stderr = proc.communicate()
+
+        #added to handle permission denied errors
+        if stderr:
+            print(f"Warning (du):\n{stderr.strip()}", file=sys.stderr)
+
+        # makes a list from the result
+        return [line.strip() for line in stdout.strip().split("\n") if line.strip()]
+    except Exception as e:
+        raise RuntimeError(f"Subprocess error: {e}")
 
 def create_dir_dict(alist):
     "gets a list from call_du_sub, returns a dictionary which should have full"
@@ -49,6 +76,7 @@ def create_dir_dict(alist):
 
 
 if __name__ == "__main__":
-    pass
-
+    args = parse_command_args()
+    dir_list = call_du_sub(args.target)
+    print(dir_list)
 
